2bf55239f4dbb1fb866cae3101be4395cc8c638f
Cory Vaughan <codercory13@gmail.com>
2017-03-16 15:50:27 -0400

    Installed jwtsimple and bcrypt.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,15 @@
 // Require your controllers here
 
 
+
+
 module.exports = (app) => {
+
+//Allows frontend to communicate to the backend
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   // Add your routes here
 };
