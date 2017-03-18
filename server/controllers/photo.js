a13b011ceeacda8d3934fbb84e216cd9109d4023
Cory Vaughan <codercory13@gmail.com>
2017-03-18 17:11:11 -0400

    Fixed bug.

--- server/controllers/photo.js
+++ server/controllers/photo.js
@@ -12,7 +12,7 @@ module.exports = {
      title: req.body.title,
      photoUrl: req.body.photoUrl,
      userId: req.user.id,
-     text: req.tags.text
+     text: req.body.text
     },
      { include: [{
           model: Tags,
