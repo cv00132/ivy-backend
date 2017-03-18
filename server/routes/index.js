ea70dc11c1b05f0b7d720dd50921d3d2207ffb60
Cory Vaughan <codercory13@gmail.com>
2017-03-18 16:29:06 -0400

    fixed access control methods.

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,7 @@ module.exports = (app) => {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
     next();
   });
   // Add your routes here
