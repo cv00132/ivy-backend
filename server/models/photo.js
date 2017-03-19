eaccb3cdea2943f09027183f3ba58d1392070119
Cory Vaughan <codercory13@gmail.com>
2017-03-18 21:05:25 -0400

    fixed 400 bad request.

--- server/models/photo.js
+++ server/models/photo.js
@@ -8,7 +8,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
        associate: function(models) {
-         Photo.belongsTo(models.User);
+         Photo.belongsTo(models.User, { foreignKey:'userId' });
     //     Photo.hasMany(models.Comments, {
     //         through: {
     //             foreignKey: 'photoId'
